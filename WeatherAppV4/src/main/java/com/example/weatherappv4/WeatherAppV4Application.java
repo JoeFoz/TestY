2c8093cff9a8a2a951ec4ff6e36560c7f60a007e
JoeFoz <josephpfoster@gmail.com>
2024-09-26 12:52:45 +0100

    push

--- WeatherAppV4/src/main/java/com/example/weatherappv4/WeatherAppV4Application.java
+++ WeatherAppV4/src/main/java/com/example/weatherappv4/WeatherAppV4Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherAppV4Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppV4Application.class, args);
 	}
