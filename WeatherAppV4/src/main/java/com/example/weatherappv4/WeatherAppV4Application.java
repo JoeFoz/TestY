93403c05c549b4d5a69a4b98ce8bffe1130afbc4
JoeFoz <josephpfoster@gmail.com>
2024-09-26 15:09:02 +0100

    space

--- WeatherAppV4/src/main/java/com/example/weatherappv4/WeatherAppV4Application.java
+++ WeatherAppV4/src/main/java/com/example/weatherappv4/WeatherAppV4Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherAppV4Application {
 
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppV4Application.class, args);
 	}
